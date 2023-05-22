c1cf2b67971563900ce7f22507a46b186cea6afc
Lays Doza <118467575+laysdoza@users.noreply.github.com>
2023-05-22 02:35:21 -0300

    Codigos melhores identados

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let menu = document.querySelector('#menu-icon')
-let navlist = document.querySelector('.navlist')
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+function openModal(imageUrl) {
+  var modal = document.getElementById("myModal");
+  var modalImage = document.getElementById("modalImage");
+  modal.style.display = "block";
+  modalImage.src = imageUrl;
 }
-    function openModal(imageUrl) {
-      var modal = document.getElementById("myModal");
-      var modalImage = document.getElementById("modalImage");
-      modal.style.display = "block";
-      modalImage.src = imageUrl;
-    }
 
-
-    function closeModal() {
-      var modal = document.getElementById("myModal");
-      modal.style.display = "none";
-    }
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
