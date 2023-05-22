00fa34a48b7b4a0c5e480e332694f72e3ac7f909
Lays Doza <118467575+laysdoza@users.noreply.github.com>
2023-05-22 02:24:42 -0300

    Adicionados arquivos da pagina

---
+++ README.md
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+# Projeto Bianca
+## Site de para um pequeno negócio de nail design.
+
+[<img src="./paginainicial.jpeg">]
+
+## Tecnologias Utilizadas
+
+- HTML 
+- CSS
+- JS
+
+## Qual intuito?
+### Com o intuito de treinar e melhorar minhas habilidades, decidi criar um site para minha tia que possuí um negócio de nail design. 💅
+=======
+# projeto-bianca
+>>>>>>> e015496d0f7db26c850724dc81218396ec43056a
---
+++ script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon')
+let navlist = document.querySelector('.navlist')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+    function openModal(imageUrl) {
+      var modal = document.getElementById("myModal");
+      var modalImage = document.getElementById("modalImage");
+      modal.style.display = "block";
+      modalImage.src = imageUrl;
+    }
+
+
+    function closeModal() {
+      var modal = document.getElementById("myModal");
+      modal.style.display = "none";
+    }
